241599ff36666763e860534282a3732022a3def8
Gordon Mackenzie-Leigh <gordon@stugo.co.uk>
2016-11-01 16:22:52 +0000

    prepare for appveyor CI

---
+++ Stugo.Logging/CallerMemberName.cs
@@ -0,0 +1,8 @@
+﻿#if !NET45
+namespace Stugo.Logging
+{
+    internal class CallerMemberName : System.Attribute
+    {
+    }
+}
+#endif
---
+++ Stugo.Logging/Properties/VersionInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+// this will be filled in by CI
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyInformationalVersion("LOCAL BUILD")]
